da699ba83dc532150ed07c4796369e88aff67d06
dmhuehol <dmhuehol@ncsu.edu>
2017-10-17 23:09:09 -0400

    Update 10/17/17
    Update covering development to surfacePlotter

--- surfacePlotter.m
+++ surfacePlotter.m
@@ -1,6 +1,7 @@
-function [] = surfacePlotter(dStart,hStart,dEnd,hEnd,ASOS)
+%function [] = surfacePlotter(dStart,hStart,dEnd,hEnd,ASOS)
 extractDays = NaN(1,length(ASOS));
-extractDays(1:end) = ASOS(1:end).Day; %Array of all days within the given structure
+extractDays = {ASOS(1:end).Day}; %Array of all days within the given structure
+extractDays = cell2mat(extractDays);
 extractDays(extractDays~=dStart & extractDays~=dEnd) = 0; %Logical indexing is v quick
 extractDays(extractDays~=0) = 1;
 dayIndices = find(extractDays~=0); %These are the indices of the input day(s)
@@ -8,5 +9,7 @@ dayIndices = find(extractDays~=0); %These are the indices of the input day(s)
 lengthDay1 = length(ASOS(dayIndices(1)));
 lengthDay2 = length(ASOS(dayIndices(2)));
 
+extractHours = NaN(1,length(ASOS));
+
 disp('Completed!')
-end
+%end
