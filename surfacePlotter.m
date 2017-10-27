47ae5e6c78b4a3efa0b8359bc317c88fa029a4bc
dmhuehol <dmhuehol@ncsu.edu>
2017-10-27 16:33:59 -0400

    Update 10/27/17
    Update covering changes to surfacePlotter and addition of external addaxis functions

--- surfacePlotter.m
+++ surfacePlotter.m
@@ -1,3 +1,7 @@
+%Requires external functions datetickzoom and addaxis
+%
+%See also datetickzoom, addaxis, addaxislabel
+%
 function [surfaceSubset] = surfacePlotter(dStart,hStart,dEnd,hEnd,ASOS)
 %% Locate the data in question
 extractDays = [ASOS.Day]; %Array of all days within the given structure, bracket is required to form an array instead of list
@@ -43,93 +47,170 @@ times = [surfaceSubset.Year; surfaceSubset.Month; surfaceSubset.Day; surfaceSubs
 serialTimes = datenum(times(1,:),times(2,:),times(3,:),times(4,:),times(5,:),times(6,:));
 
 presentWeather = {surfaceSubset.PresentWeather}; %Weather codes
-%weatherPresence = logical(~cellfun('isempty',presentWeather)); %Logically index the present weather codes; no weather code (0)/weather code (1)
-%precipTimes = [time(2) time(3) time(4)]; %Times where precip occurred
-%precipType = {'SN','RA','SNRA'}; %Snow at first, rain at second, then both
-
-% Place precipitation type markers at top of plot
-% for count = 1:length(precipType) %Loop to look for all precipitation type markers
-%     precipPoint = plot(precipTimes(count),100,'Marker','o','MarkerSize',5,'MarkerFaceColor','w'); %Invisible at first
-%     if strcmp(precipType{count},'SNRA') == 1 %If snow and rain
-%         set(precipPoint,'MarkerFaceColor','b')
-%         set(precipPoint,'Marker','p') %Blue pentagons
-%     elseif strcmp(precipType{count},'SN')==1 %If snow
-%         set(precipPoint,'MarkerFaceColor','k') %Black stars
-%         set(precipPoint,'Marker','*')
-%         set(precipPoint,'MarkerSize',7) %Make a little larger because stars are hard to see
-%     elseif strcmp(precipType{count},'RA')==1 %If rain
-%         set(precipPoint,'MarkerFaceColor','b') %Blue circles
-%     end
-%     hold on
-% end
+fogchk = 0; frzfogchk = 0; mistchk = 0; rainchk = 0; frzdrizchk = 0; drizchk = 0; frzrainchk = 0;
+sleetchk = 0; graupchk = 0; snowchk = 0; upchk = 0; hailchk = 0; icchk = 0; thunderchk = 0;
+yplacer = 0;
 figure;
 presentAxis = gca;
-for count = 1:length(presentWeather)
+for count = 1:length(presentWeather) %Sometimes you just have to get loopy
     if isempty(regexp(presentWeather{count},'(FG){1}','once'))~=1
-        plot(serialTimes(count),1,'b');
+        if fogchk==0
+            fogplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Fog'; %#ok
+            fogchk=1;
+        end
+        plot(serialTimes(count),fogplace,'b','Marker','.','MarkerEdgeColor',[128 128 128]./255,'MarkerSize',5);
+        hold on
+    end
+    if isempty(regexp(presentWeather{count},'(FZFG){1}','once'))~=1
+        if frzfogchk==0
+            frzfogplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Frz Fog'; %#ok
+            frzfogchk=1;
+        end
+        plot(serialTimes(count),frzfogplace,'b','Marker','.','MarkerEdgeColor',[128 128 128]./255,'MarkerSize',5);
         hold on
     end
     if isempty(regexp(presentWeather{count},'(BR){1}','once'))~=1
-        plot(serialTimes(count),2,'b');
+        if mistchk==0
+            mistplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Mist';
+            mistchk=1;
+        end
+        plot(serialTimes(count),mistplace,'b','Marker','.','MarkerEdgeColor',[128 128 128]./255,'MarkerSize',5);
         hold on
     end
     if isempty(regexp(presentWeather{count},'(DZ){1}','once'))~=1 && isempty(regexp(presentWeather{count},'(FZDZ){1}','once'))==1
-        plot(serialTimes(count),3,'k');
+        if drizchk==0
+            dzplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Drizzle';
+            drizchk=1;
+        end
+        plot(serialTimes(count),dzplace,'k','Marker','.','MarkerSize',8);
         hold on
     end
-%     if isempty(regexp(presentWeather{count},'(FZDZ){1}','once'))~=1
-        plot(serialTimes(count),4,'k');
+     if isempty(regexp(presentWeather{count},'(FZDZ){1}','once'))~=1
+        if frzdrizchk==0
+            fzdzplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Frz Drizzle';
+            frzdrizchk=1;
+        end
+        plot(serialTimes(count),fzdzplace,'k','Marker','.','MarkerSize',8);
         hold on
     end
     if isempty(regexp(presentWeather{count},'(RA){1}','once'))~=1 && isempty(regexp(presentWeather{count},'(FZRA){1}','once'))==1
-        plot(serialTimes(count),5,'Marker','.','MarkerFaceColor','b');
+        if rainchk==0
+            rainplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Rain';
+            rainchk=1;
+        end
+        plot(serialTimes(count),rainplace,'Marker','.','MarkerFaceColor','b','MarkerSize',12);
         hold on
     end
     if isempty(regexp(presentWeather{count},'(FZRA){1}','once'))~=1
-        plot(serialTimes(count),6,'Marker','.','MarkerFaceColor','b');
+        if frzrainchk==0
+            fzraplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Frz Rain';
+            frzrainchk=1;
+        end
+        plot(serialTimes(count),fzraplace,'Marker','.','MarkerFaceColor','b','MarkerSize',12);
         hold on
     end
-    if isempty(regexp(presentWeather{count},'(PL){1}','once'))~=1 %|| isempty(regexp(presentWeather{count},'(PE){1}','once'))~=1
-        plot(serialTimes(count),7,'o','MarkerEdgeColor',[128 128 128]./255,'MarkerFaceColor',[128 128 128]./255);
+    if isempty(regexp(presentWeather{count},'(PL){1}','once'))~=1 || isempty(regexp(presentWeather{count},'(PE){1}','once'))~=1
+        if sleetchk==0
+            sleetplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Sleet';
+            sleetchk=1;
+        end
+        plot(serialTimes(count),sleetplace,'o','MarkerEdgeColor',[128 128 128]./255,'MarkerFaceColor',[128 128 128]./255,'MarkerSize',8);
         hold on
     end
-    if isempty(regexp(presentWeather{count},'(SG){1}','once'))~=1
-        plot(serialTimes(count),8,'Marker','*','MarkerEdgeColor','g');
+    if isempty(regexp(presentWeather{count},'(SG){1}','once'))~=1 || isempty(regexp(presentWeather{count},'(GS){1}','once'))~=1
+        if graupchk==0
+            graupplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Graupel';
+            graupchk=1;
+        end
+        plot(serialTimes(count),graupplace,'Marker','*','MarkerEdgeColor','g');
         hold on
     end
     if isempty(regexp(presentWeather{count},'(SN){1}','once'))~=1
-        plot(serialTimes(count),9,'Marker','*','MarkerEdgeColor','c');
+        if snowchk==0
+            snowplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Snow';
+            snowchk=1;
+        end
+        plot(serialTimes(count),snowplace,'Marker','*','MarkerEdgeColor','c');
+        hold on
+    end
+    if isempty(regexp(presentWeather{count},'(IC){1}','once'))~=1
+        if icchk==0
+            icplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Ice Crystals';
+            icchk=1;
+        end
+        plot(serialTimes(count),icplace,'Marker','-','MarkerEdgeColor',[128 128 128]./255,'MarkerSize',8);
+        hold on
+    end
+    if isempty(regexp(presentWeather{count},'(GR){1}','once'))~=1
+        if hailchk==0
+            hailplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Hail';
+            hailchk=1;
+        end
+        plot(serialTimes(count),hailplace,'Marker','o','MarkerEdgeColor','r','MarkerFaceColor','r','MarkerSize',8);
+        hold on
+    end
+    if isempty(regexp(presentWeather{count},'(TS){1}','once'))~=1
+        if thunderchk==0
+            thunderplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Thunderstorm';
+            thunderchk=1;
+        end
+        plot(serialTimes(count),thunderplace,'Marker','d','MarkerEdgeColor','y','MarkerFaceColor','y','MarkerSize',6);
+        hold on
+    end
+    if isempty(regexp(presentWeather{count},'(UP){1}','once'))~=1
+        if upchk==0
+            upplace = yplacer+1;
+            yplacer = yplacer+1;
+            presentLabels{yplacer} = 'Uknwn Precip';
+            upchk=1;
+        end
+        plot(serialTimes(count),upplace,'Marker','p','MarkerEdgeColor','m','MarkerFaceColor','m','MarkerSize',6);
+        hold on
     end
     hold on
 end
-ylim([0 10]);
-set(presentAxis,'YTick',[1 2 3 4 5 6 7 8 9]);
-set(presentAxis,'YTickLabel',{'Fog','Mist','Drizzle','Frz Drizzle','Rain','Frz Rain','Sleet','Graupel','Snow'});
-datetick('x')
+ylim([0 yplacer+1]);
+set(presentAxis,'YTick',1:yplacer);
+set(presentAxis,'YTickLabel',presentLabels);
+title('Precip Type')
+datetickzoom('x')
 hold off
 
 figure;
-[ax,hlines] = plotyyy(serialTimes,humidity,serialTimes,pressure,serialTimes,tTd); %Requires secondary function plotyyy
-datetick(ax(1)) %Date tick on x axis
-datetick(ax(2))
-datetick(ax(3))
-set(hlines(1),'Color',[204 0 0]./255) %Humidity color
-set(hlines(1),'LineWidth',1.2)
-set(ax(1),'ycolor',[204 0 0]./255) %Humidity axis
-ylabel(ax(1),'%')
-set(hlines(2),'Color',[0 0 223]./255); %Pressure color
-set(hlines(2),'LineWidth',1.2)
-set(ax(2),'ycolor',[0 0 223]./255); %Pressure axis
-ylabel(ax(2),'hPa')
-set(hlines(3),'Color',[0 0 0]./255); %deg C color
-set(hlines(3),'LineWidth',1.2)
-set(ax(3),'ycolor',[0 0 0]./255) %deg C axis
-ylabel(ax(3),'deg C')
-set(hlines(4),'Color',[0 255 0]./255); %deg C color
-set(hlines(4),'LineWidth',1.2)
-legend(hlines,'Humidity','Pressure','Temperature','Dewpoint');
-set(ax(2),'XTick',[]) %Disable other x axis tick marks
-set(ax(3),'XTick',[])
+tempdew = plot(serialTimes,tTd);
+ylabel('deg C')
+addaxis(serialTimes,pressure,'r');
+addaxislabel(2,'hPa')
+addaxis(serialTimes,humidity,'m');
+addaxislabel(3,'%')
+legend('Temperature','Dewpoint','Pressure','Humidity');
+datetickzoom
 
 titleString = 'Surface observations data for ';
 toString = 'to';
@@ -159,10 +240,6 @@ maxWind = max(windSpeed); %Maximum entry is maximum radius of compass
 figure; %New figure
 hiddenArrow = compass(maxWind); %Need to set maximum compass size at beginning, otherwise large arrows will size off the screen
 set(hiddenArrow,'Visible','off');
-% dateStrings = cell(1,length(serialTimes));
-% for datescount = 1:length(serialTimes)
-%     dateStrings{datescount} = datestr(serialTimes(datescount)); %Need date strings to label the color bar
-% end
 hold on
 for windC = 1:15:numWind %Plot vector iteratively
     h = compass(windU(windC),windV(windC)); %Time is visible as color
