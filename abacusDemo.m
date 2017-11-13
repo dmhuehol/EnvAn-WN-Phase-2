296cf6c21eceb504801216584fe8517dbcfeac53
dmhuehol <dmhuehol@ncsu.edu>
2017-11-13 14:52:31 -0500

    Daily Update 11/13/17
    Update covering further development of newNoseDetect and abacusDemo; surfacePlotter becomes operational. Removes Storm List 1 and adds soundings to the Station List.

--- abacusDemo.m
+++ abacusDemo.m
@@ -1,5 +1,20 @@
 %%abacusDemo
-    %demonstrates abacus plot creation for present weathers
+    %demonstrates abacus plot creation for present weather codes
     %10/23/17
     %Daniel Hueholt
-figure; plot(1,1,'*'); hold on; plot(1,2,'*'); hold on; plot(1,3,'*'); asdf = gca; set(asdf,'YTick',[1 2 3]); set(asdf,'YTickLabel',{'Rain','Snow','Sleet'});
+figure;
+plot(1,1,'*'); 
+hold on;
+plot(1,2,'*'); 
+hold on; 
+plot(1,3,'*'); 
+hold on
+plot(2,1,'*');
+hold on
+plot(3,1,'*');
+asdf = gca;
+set(asdf,'YTick',[1 2 3]);
+set(asdf,'YTickLabel',{'Rain','Snow','Sleet'});
+ylim([0 4])
+xlim([0 4])
+title('Abacus plot for weather codes - uses fake data')
--- newNoseDetect.m
+++ newNoseDetect.m
@@ -3,12 +3,37 @@
 
 numSound = length(goodfinal);
 
-for soundCount = 4297:length(numSound)
+for soundCount = 1:numSound
     logicalTemp = logical(goodfinal(soundCount).temp>0); %Logically index on temperatures greater than 0
     if isempty(nonzeros(logicalTemp))==1 %If there are none
         continue %This is not a warm nose sounding, continue looping
     end
     warmInd = find(logicalTemp==1); %Isolate the indices that correspond to superfreezing temperatures
     breakInd = find(diff(warmInd)~=1); %This identifies any skips in the indices, which correspond to multiple noses
+    numBreak = length(breakInd);
+    if numBreak==0 %No break implies single nose
+        noseInd = warmInd;
+    elseif numBreak==1 %One break implies two noses
+        firstNoseInd = warmInd(1):warmInd(breakInd);
+        secondNoseInd = warmInd(breakInd+1):warmInd(end);
+    elseif numBreak==2
+        firstNoseInd = warmInd(1):warmInd(breakInd(1));
+        secondNoseInd = warmInd(breakInd(1)+1):warmInd(breakInd(2));
+        thirdNoseInd = warmInd(breakInd(2)+1):warmInd(end);
+    elseif numBreak==3
+        firstNoseInd = warmInd(1):warmInd(breakInd(1));
+        secondNoseInd = warmInd(breakInd(1)+1):warmInd(breakInd(2));
+        thirdNoseInd = warmInd(breakInd(2)+1):warmInd(3);
+        fourthNoseInd = warmInd(breakInd(3)+1):warmInd(end);
+    elseif numBreak==4
+        firstNoseInd = warmInd(1):warmInd(breakInd(1));
+        secondNoseInd = warmInd(breakInd(1)+1):warmInd(breakInd(2));
+        thirdNoseInd = warmInd(breakInd(2)+1):warmInd(3);
+        fourthNoseInd = warmInd(breakInd(3)+1):warmInd(4);
+        fifthNoseInd = warmInd(breakInd(4)+1):warmInd(end);
+    elseif numBreak>4
+        disp(soundCount)
+        disp('Are you serious')
+    end
     
 end
